b556c551a551e30906e10ad2c4e132896c062665
yunsuklee <sergioleesp@gmail.com>
2022-11-14 11:29:02 +0100

    feat: home, admin component and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+
+/* 
+  Routes need be an array of objects where each object
+  has a 'path' key whose value is the name of the link
+  and the 'component' key whose value is the actual component
+  that you're linking to
+*/
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
